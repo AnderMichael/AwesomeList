1d734f4041e187374b3e01e09576e534bc21c68a
AnderMichael <andercaymi@gmail.com>
2023-02-07 08:51:05 -0400

    Add constants

--- src/themes/global.ts
+++ src/themes/global.ts
@@ -1,11 +1,19 @@
 import {StyleSheet} from 'react-native';
+import {colorsApp} from '../constants/colorsApp';
+// ! This is the palette color https://www.instagram.com/p/CloCdxBJzs9/?hl=es-la
+
 export const globalStyles = StyleSheet.create({
   backgroundApp: {
     flex: 1,
-    backgroundColor: 'grey',
-    padding: 10,
+    backgroundColor: colorsApp.YELLOW_COLOR,
+    padding: 20,
     alignItems: 'stretch',
-    justifyContent: 'space-around',
-    margin: 5
+    justifyContent: 'space-evenly',
+  },
+  title: {
+    fontFamily: 'JosefinSans-SemiBold',
+    textAlign: 'center',
+    fontSize: 25,
+    color: colorsApp.RED_COLOR,
   },
 });
