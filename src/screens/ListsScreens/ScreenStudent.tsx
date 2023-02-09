e481774c2ff2080c4b0a079f3d329caf8e3460fa
AnderMichael <andercaymi@gmail.com>
2023-02-09 09:41:09 -0400

    Final

--- src/screens/ListsScreens/ScreenStudent.tsx
+++ src/screens/ListsScreens/ScreenStudent.tsx
@@ -56,6 +56,7 @@ export const ScreenStudent = ({route}:any) => {
           nextStudent={nextStudent}
           previousStudent={previousStudent}
           studentsNumber={studentsListLength}
+          list={attendanceList}
         />
       )}
     </>
